44500f7ae7821aae70546b28b9330dfd43f865fa
dpeedah <dan.peedah@gmail.com>
2021-06-04 08:43:25 +0100

    First push initializing the home page and repository

---
+++ src/test/java/pages/CarTaxHomePage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+// representing cartaxcheck.co.uk
+public class CarTaxHomePage {
+
+    WebDriver driver;
+    // input field for rg
+    By regTextField = By.name("vrm");
+
+    // get full check 'button'
+    By fullCheckLink = By.linkText("Get a Full Check");
+
+    // get free check button
+    By freeCheckBtn = By.xpath("//button[contains(text(),'Free Car Check')]");
+
+    // get x off MOT with fixter link
+    By getMoneyOff = By.linkText("Get £10 off MOT or Servicing with Fixter");
+
+    //mot check link
+    By motCheckRelink = By.cssSelector("a[href=/mot-check/]");
+
+    // car history check link
+    By carHistoryCheck = By.cssSelector("a[href=/car-history-check/]");
+
+    public CarTaxHomePage(WebDriver driver){
+        this.driver = driver;
+    }
+}
